446a50fc93261dd79a882ddfad4034c73bdf79e7
drifterz13 <mixmyxxe@gmail.com>
2018-10-15 22:02:58 +0700

    fix form submission issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -103,7 +103,7 @@ class Contact extends React.Component {
     handleSubmit = e => {
         e.preventDefault();
         const form = e.target;
-        fetch("/", {
+        fetch("/contact?no-cache=1", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: encode({
